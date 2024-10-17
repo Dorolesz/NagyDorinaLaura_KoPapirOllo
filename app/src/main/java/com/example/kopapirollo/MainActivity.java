567d9eb232cc53670774c9e5aa6482696f010d15
Petrik Diak <diak@petrik.hu>
2024-10-17 15:33:15 +0200

    init

---
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.kopapirollo;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button Ko, Papir, Ollo;
+    private TextView Player, Computer;
+    private Random random;
+    String[] choices = {"Kő", "Papír", "Olló"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Ko = findViewById(R.id.koButton);
+        Papir = findViewById(R.id.papirButton);
+        Ollo = findViewById(R.id.olloButton);
+        Player = findViewById(R.id.teValasztasod);
+        Computer = findViewById(R.id.gepValasztasa);
+
+        Ko.setOnClickListener(view -> Game("Kő"));
+        Papir.setOnClickListener(view -> Game("Papír"));
+        Ollo.setOnClickListener(view -> Game("Olló"));
+
+        public void Game(String playerChoice) {
+            String computerChoice = choices[random.nextInt(3)];
+
+            if (playerChoice.equals(computerChoice)) {
+                eredmeny.setText("Döntetlen");}
+                else if ((playerChoice.equals("Kő") && computerChoice.equals("Olló")) ||
+                    (playerChoice.equals("Papír") && computerChoice.equals("Kő")) ||
+                    (playerChoice.equals("Olló") && computerChoice.equals("Papír")) {
+                eredmeny.setText("Nyertél!");
+            } else {
+            eredmeny.setText("Vesztettél");}
+
+        }
+
+
+    }
+}
